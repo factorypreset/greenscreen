d09c9785370c4353b44ceb55dbca2ff0d8ec84d9
Bo Daley <bo@factorypreset.com>
2013-03-16 16:10:07 -0400

    empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zipgreen::Application.config.secret_token = '2aab9ef09df41ef2b6a54d5723647408771f16cd7757fa404dd91fa0a04271965362cb6bc5063c46b8dcc06b9806f66cac7a6a3d932780b7425bc9445d5c1b5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Zipgreen::Application.config.session_store :cookie_store, key: '_zipgreen_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Zipgreen::Application.config.session_store :active_record_store
