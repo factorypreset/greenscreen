e4ee2f7edb5d602a2175327756376b546c9bc683
Bo Daley <bo@factorypreset.com>
2014-04-27 06:18:52 -0400

    get statistics for city_mpg too

--- app/models/vehicle_make.rb
+++ app/models/vehicle_make.rb
@@ -5,7 +5,15 @@ class VehicleMake < ActiveRecord::Base
     VehicleModel.where(:vehicle_make_id => id).where(:model_year => year).average(:combined_mpg)
   end
 
+  def average_city_mpg_for_year(year)
+    VehicleModel.where(:vehicle_make_id => id).where(:model_year => year).average(:city_mpg)
+  end
+
   def average_combined_mpg_by_year
     VehicleModel.where(:vehicle_make_id => id).group(:model_year).average(:combined_mpg)
+  end
+
+  def average_city_mpg_by_year
+    VehicleModel.where(:vehicle_make_id => id).group(:model_year).average(:city_mpg)
   end
 end
--- test/models/vehicle_make_test.rb
+++ test/models/vehicle_make_test.rb
@@ -19,19 +19,25 @@ describe VehicleMake do
                               model: "First Model",
                               model_year: "2013",
                               vehicle_make_id: vehicle_make.id,
-                              combined_mpg: 10
+                              combined_mpg: 10,
+                              city_mpg: 8,
+                              hwy_mpg: 12
 
       vehicle_model2 = create :vehicle_model,
                               model: "Second Model",
                               model_year: "2013",
                               vehicle_make_id: vehicle_make.id,
-                              combined_mpg: 30
+                              combined_mpg: 30,
+                              city_mpg: 20,
+                              hwy_mpg: 40
 
       vehicle_model3 = create :vehicle_model,
                               model: "Third Model",
                               model_year: "2014",
                               vehicle_make_id: vehicle_make.id,
-                              combined_mpg: 100
+                              combined_mpg: 100,
+                              city_mpg: 80,
+                              hwy_mpg: 120
 
       vehicle_model1.save!
       vehicle_model2.save!
@@ -54,6 +60,21 @@ describe VehicleMake do
       avg[2014].to_i.must_equal 100
     end
 
+    it "can calculate average city mpg for a single year" do
+      avg = vehicle_make.average_city_mpg_for_year("2014")
+      avg.must_equal 80
+    end
+
+    it "can calculate average city mpg for two vehicles in the same year" do
+      avg = vehicle_make.average_city_mpg_for_year("2013")
+      avg.must_equal 14
+    end
+
+    it "can calculate average city mpg across years" do
+      avg = vehicle_make.average_city_mpg_by_year
+      avg[2013].to_i.must_equal 14
+      avg[2014].to_i.must_equal 80
+    end
   end
 
 end
