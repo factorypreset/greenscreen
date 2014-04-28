cd2db9ba9b90d75f48d0ac7c0409931f9a773d33
Bo Daley <bo@factorypreset.com>
2014-04-28 07:31:32 -0400

    add some more fields to statistics response

--- app/controllers/vehicle_makes_controller.rb
+++ app/controllers/vehicle_makes_controller.rb
@@ -23,7 +23,13 @@ class VehicleMakesController < ApplicationController
   # GET /vehicle_makes/:id/statistics
   def statistics
     @vehicle_make = VehicleMake.find(params[:id])
-    @vehicle_statistics = @vehicle_make.averages_by_year([:city_mpg, :hwy_mpg, :combined_mpg])
+    @vehicle_statistics = @vehicle_make.averages_by_year([
+      :city_mpg,
+      :hwy_mpg,
+      :combined_mpg,
+      :air_pollution_score,
+      :greenhouse_gas_score
+    ])
     respond_to do |format|
       format.json { render json: @vehicle_statistics }
       format.any
--- test/controllers/vehicle_makes_controller_test.rb
+++ test/controllers/vehicle_makes_controller_test.rb
@@ -41,16 +41,32 @@ describe VehicleMakesController do
       @model = create :vehicle_model,
                       model: "Prius",
                       model_year: "2013",
+                      city_mpg: 30,
+                      hwy_mpg: 50,
                       combined_mpg: 40,
+                      air_pollution_score: 6,
+                      greenhouse_gas_score: 7,
                       vehicle_make_id: @make.id
-    end
 
-    it "can get average combined mpg per year for a given vehicle make" do
       get :statistics, format: 'json', id: @make.id
       response.success?.must_equal true
+      @result = JSON.parse(response.body)
+    end
 
-      result = JSON.parse(response.body)
-      result['combined_mpg']['2013'].to_i.must_equal 40
+    it "returns a full set of statistics for a given vehicle make" do
+      @result.has_key?('city_mpg').must_equal true
+      @result.has_key?('hwy_mpg').must_equal true
+      @result.has_key?('combined_mpg').must_equal true
+      @result.has_key?('air_pollution_score').must_equal true
+      @result.has_key?('greenhouse_gas_score').must_equal true
+    end
+
+    it "only returns valid statistics" do
+      @result.has_key?('non_existent_statistic').must_equal false
+    end
+
+    it "can get correct average value for one field" do
+      @result['combined_mpg']['2013'].to_i.must_equal 40
     end
   end
 end
